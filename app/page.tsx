369edb9df17734ad7b63bfcfee3ae52c274c54ad
ic768 <ic768@protonmail.com>
2024-07-27 22:02:35 +0300

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-      Init
-    </main>
+    <>
+      <header className="flex justify-between w-full py-4 px-6 ">
+        <div className="">Ioannis Chrysostomakis</div>
+        <Link href="/">About Me</Link>
+      </header>
+
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <span>
+          Hello! My name is Ioannis Chrysostomakis, but I&apos;m also called
+          Giannis or John in English 🙂
+        </span>
+      </main>
+    </>
   );
 }
