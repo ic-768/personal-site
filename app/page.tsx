d170d9c23e35a36c678293ab30dc4239d04d2b2d
ic768 <ic768@protonmail.com>
2024-07-27 22:35:33 +0300

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header className="flex justify-between w-full py-4 px-6 ">
-        <div className="">Ioannis Chrysostomakis</div>
+      <header className="flex w-full py-4 px-6">
         <Link href="/">About Me</Link>
       </header>
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center p-24">
         <span>
-          Hello! My name is Ioannis Chrysostomakis, but I&apos;m also called
-          Giannis or John in English 🙂
+          Hello! My name is{" "}
+          <span className="font-bold">Ioannis Chrysostomakis,</span>
+        </span>
+        <span>
+          but I&apos;m also called <span className="font-bold">Giannis</span> or{" "}
+          <span className="font-bold">John</span> in English 🙂
+        </span>
+
+        <span>
+          I&apos;m a front-end developer with more than{" "}
+          <span className="font-bold">3 years</span> of professional experience,
+          mainly working on building <span className="font-bold">React</span>{" "}
+          applications.
         </span>
       </main>
     </>
